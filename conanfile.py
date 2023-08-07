d0c63685eaad8ce19e0a48cd2365a52019960d3f
Jaibeer  S Dugal <jaibeer72@gmail.com>
2023-08-07 16:40:24 +0100

    something working

--- conanfile.py
+++ conanfile.py
@@ -1,22 +1,17 @@
 from conan import ConanFile
 from conan.tools.cmake import CMakeToolchain, CMake, cmake_layout
-from conan.tools.apple import XcodeDeps
-from conan.tools.apple import XcodeBuild
 
 class YourProjectConan(ConanFile):
     name = "Cross-Platform-VulkanGFX"
     version = "0.0.1b"
     settings = "os", "compiler", "build_type", "arch"
     requires = [
-        "glfw/3.3.2",     # Replace with the version you need.
+        "vulkan-loader/1.3.204.1",     # Replace with the version you need.
         "glm/cci.20230113"     # Replace with the version you need.
     ]
-    generators = "CMakeDeps", "CMakeToolchain"
-
-    def generate(self):
-        xcode = XcodeDeps(self)
-        xcode.generate()
+    generators = "CMakeDeps", "CMakeToolchain" 
 
     def build(self):
-        xcodebuild = XcodeBuild(self)
-        xcodebuild.build("app.xcodeproj")
+        cmake = CMake(self)
+        cmake.configure()
+        cmake.build()
