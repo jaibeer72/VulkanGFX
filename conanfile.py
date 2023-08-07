01ff97b69a8a25a8648bb1661d2949320bb09a5c
Jaibeer  S Dugal <jaibeer72@gmail.com>
2023-08-07 23:16:12 +0100

    trying the latest version of conen package for vulkan

--- conanfile.py
+++ conanfile.py
@@ -6,7 +6,7 @@ class YourProjectConan(ConanFile):
     version = "0.0.1b"
     settings = "os", "compiler", "build_type", "arch"
     requires = [
-        "vulkan-loader/1.3.204.1",     # Replace with the version you need.
+        "vulkan-loader/1.3.239.0",     # Replace with the version you need.
         "glm/cci.20230113"  ,   # Replace with the version you need.
         "glfw/3.3.8"
     ]
