2034ee694e7611260cf8c43e22a568966b8c1175
Jaibeer  S Dugal <jaibeer72@gmail.com>
2023-08-08 04:46:45 +0100

    dubuggin failure of SLN creation

--- MakeProject.bat
+++ MakeProject.bat
@@ -12,22 +12,35 @@
 @REM     python get-pip.py
 @REM )
 
-@REM Check if conan profile is exist
+REM Check if conan profile exists
 conan profile detect
-@REM run conan install command
-conan install . -s build_type=Debug --output-folder=ProjectFolder --build missing
+if %ERRORLEVEL% neq 0 (
+    echo Conan profile detection failed.
+    exit /b %ERRORLEVEL%
+)
 
-@REM check if install was sucessful
+REM Run conan install command
+conan install . -s build_type=Debug --output-folder=ProjectFolder --build missing
+if %ERRORLEVEL% neq 0 (
+    echo Conan install failed.
+    exit /b %ERRORLEVEL%
+)
+
+REM Check if install was successful
 if not exist ProjectFolder (
-    echo conan install was not sucessful. 
+    echo Conan install was not successful.
     pause
-    exit
+    exit /b 1
 )
 
 cd ProjectFolder
 
 REM Generate the Visual Studio solution
-cmake .. -DCMAKE_TOOLCHAIN_FILE=conan_toolchain.cmake -G "Visual Studio 17 2022" 
+cmake .. -DCMAKE_TOOLCHAIN_FILE=conan_toolchain.cmake -G "Visual Studio 17 2022"
+if %ERRORLEVEL% neq 0 (
+    echo CMake command failed.
+    exit /b %ERRORLEVEL%
+)
 
 echo Visual Studio solution generated in the build directory.
 pause
