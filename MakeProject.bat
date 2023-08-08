a4a2c23abbb2190e45dfddfaf959d4dec8a63c48
Jaibeer  S Dugal <jaibeer72@gmail.com>
2023-08-08 04:25:58 +0100

    Making a visual studio solution helpers

--- MakeProject.bat
+++ MakeProject.bat
@@ -1,11 +1,46 @@
 @echo off
 
-REM Create the build directory if it doesn't exist
-if not exist build mkdir build
-cd build
+@REM REM Create the build directory if it doesn't exist
+@REM if not exist build mkdir build
+@REM cd build
+
+@REM check if pip is installed 
+if not exist pip (
+    echo pip is not installed. 
+    @REM install pip 
+    curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+    python get-pip.py
+)
+
+@REM Check if conan is installed
+if not exist conan (
+    echo conan is not installed. 
+    @REM install conan 
+    pip install conan
+)
+@REM Check if conan is installed sucessfully
+if not exist conan (
+    echo conan is not installed sucessfully. 
+    pause
+    exit
+)
+
+@REM Check if conan profile is exist
+conan profile detect
+@REM run conan install command
+conan install . -s build_type=Debug -c tools.cmake.cmaketoolchain:generator="Visual Studio 17 2022" --output-folder=ProjectFolder --build missing
+
+@REM check if install was sucessful
+if not exist ProjectFolder (
+    echo conan install was not sucessful. 
+    pause
+    exit
+)
+
+cd ProjectFolder
 
 REM Generate the Visual Studio solution
-cmake -G "Visual Studio 17 2022" ..
+cmake .. -DCMAKE_TOOLCHAIN_FILE=conan_toolchain.cmake -G "Visual Studio 17 2022" ..
 
 echo Visual Studio solution generated in the build directory.
 pause
--- ReadMe.md
+++ ReadMe.md
@@ -14,8 +14,15 @@
 - Cmake 
 - Conan 
 
+# How to build 
+## requirements 
+- cmake 
+- pip / will be installed by the run.sh file if possioble not considered all edge cases yes 
 
-# MacOsBuild
+## how to build the Xcode project 
+- in the folder running the command ```source MakeProject.sh``` should give you an easy way to build the project 
+
+# Build status on main
 
 ![Mac OS Xcode File gen and Build exe](https://github.com/jaibeer72/VulkanGFX/actions/workflows/build_Xcode_Proj.yml/badge.svg?branch=main)
 ![Windows VS17(2022) SLN gen and Build exe](https://github.com/jaibeer72/VulkanGFX/actions/workflows/build_VS_Solution.yml/badge.svg?branch=main)
