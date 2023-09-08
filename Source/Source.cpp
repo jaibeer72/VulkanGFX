f3e1e76633d94b8ebd3a66d4f6e47f9e8c532a12
Jaibeer  S Dugal <jaibeer72@gmail.com>
2023-09-08 13:15:48 +0100

    Simlifying CMake Lists

--- Source/Source.cpp
+++ Source/Source.cpp
@@ -1,6 +1,7 @@
 #define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
 
+
 #include <iostream>
 #include <stdexcept>
 #include <cstdlib>
--- conanfile.py
+++ conanfile.py
@@ -1,28 +1,24 @@
 from conan import ConanFile
-from conan.tools.cmake import CMakeToolchain, CMake, cmake_layout
+from conan.tools.cmake import CMakeToolchain, CMake
 from conan.tools.apple import fix_apple_shared_install_name
 
 class VulkanGFXConan(ConanFile):
     name = "Cross-Platform-VulkanGFX"
     version = "0.0.1b"
     settings = "os", "compiler", "build_type", "arch"
-    generators = "CMakeDeps", "CMakeToolchain" 
+    generators = "CMakeDeps", "CMakeToolchain"
+    options = {"shared": [True, False]}
     default_options = {
-        "moltenvk/*:shared": True,
-        "glm/*:shared": True,
-        "glfw/*:shared": True,
-        "vulkan-loader/*:shared": True,
+        "shared": True,
     }
-    
+
     def requirements(self):
+        self.requires("glm/cci.20230113")
+        self.requires("glfw/3.3.8")
         if self.settings.os == "Macos":
             self.requires("moltenvk/1.2.2")
-            self.requires("glm/cci.20230113")
-            self.requires("glfw/3.3.8")
         else:
             self.requires("vulkan-loader/1.3.239.0")
-            self.requires("glm/cci.20230113")
-            self.requires("glfw/3.3.8")
 
     def build(self):
         cmake = CMake(self)
@@ -32,7 +28,7 @@ class VulkanGFXConan(ConanFile):
     def package(self):
         if self.settings.os == "Macos":
             fix_apple_shared_install_name(self)
-            
+        
         self.copy("*.h", dst="include", src="src")
         self.copy("*.lib", dst="lib", keep_path=True)
         self.copy("*.dll", dst="bin", keep_path=True)
